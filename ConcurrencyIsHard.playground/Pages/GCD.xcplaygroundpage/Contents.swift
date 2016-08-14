00c08ab0ad1c5f64d9cc44a469c4450c694af0fd
Sam Davies <sam@razeware.com>
2016-08-14 23:36:18 +0200

    [Phase 6] GCD Queues

--- ConcurrencyIsHard.playground/Pages/GCD.xcplaygroundpage/Contents.swift
+++ ConcurrencyIsHard.playground/Pages/GCD.xcplaygroundpage/Contents.swift
@@ -11,13 +11,14 @@ PlaygroundPage.current.needsIndefiniteExecution = true
  ### Using a Global Queue
  iOS has some global queues, where every task eventually ends up being executed. You can use these directly. You need to use the main queue for UI updates.
  */
-// TODO
+let queue = DispatchQueue.global(qos: DispatchQoS.QoSClass.userInitiated)
+let mainQueue = DispatchQueue.main
 
 //: ### Creating your own Queue
 //: Creating your own queues allow you to specify a label, which is super-useful for debugging.
 //: You can specify whether the queue is serial (default) or concurrent (see later).
 //: You can also specify the QOS or priority (here be dragons)
-// TODO
+let workerQueue = DispatchQueue(label: "com.raywenderlich.worker", qos: .userInitiated)
 
 //: ### Getting the queue name
 //: You can't get hold of the "current queue", but you can obtain its name - useful for debugging
@@ -34,7 +35,9 @@ print(currentQueue)
 //: ### Dispatching work asynchronously
 //: Send some work off to be done, and then continue on—don't await a result
 print("=== Sending asynchronously to Worker Queue ===")
-// TODO
+workerQueue.async {
+  print("=== ASYNC:: Executing on \(currentQueueName()) ===")
+}
 print("=== Completed sending asynchronously to worker queue ===\n")
 
 
@@ -42,7 +45,9 @@ print("=== Completed sending asynchronously to worker queue ===\n")
 //: ### Dispatching work synchronously
 //: Send some work off and wait for it to complete before continuing (here be more dragons)
 print("=== Sending SYNChronously to Worker Queue ===")
-// TODO
+workerQueue.sync {
+  print("=== SYNC:: Executing on \(currentQueueName()) ===")
+}
 print("=== Completed sending synchronously to worker queue ===\n")
 
 
@@ -55,11 +60,21 @@ func doComplexWork() {
 }
 
 print("=== Starting Serial ===")
-// TODO
+workerQueue.async(execute: doComplexWork)
+workerQueue.async(execute: doComplexWork)
+workerQueue.async(execute: doComplexWork)
+workerQueue.async(execute: doComplexWork)
 
 sleep(5)
 
-// TODO
+
+let concurrentQueue = DispatchQueue(label: "com.raywenderlich.concurrent", qos: .userInitiated, attributes: .concurrent)
+
+print("\n=== Starting concurrent ===")
+concurrentQueue.async(execute: doComplexWork)
+concurrentQueue.async(execute: doComplexWork)
+concurrentQueue.async(execute: doComplexWork)
+concurrentQueue.async(execute: doComplexWork)
 
 
 sleep(5)
