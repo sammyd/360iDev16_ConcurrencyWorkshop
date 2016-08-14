62eaecbd9f086a16cb728cd032636065b171d364
Sam Davies <sam@razeware.com>
2016-08-14 23:38:33 +0200

    [Phase 7] Dispatch Groups

--- ConcurrencyIsHard.playground/Pages/Dispatch Groups.xcplaygroundpage/Contents.swift
+++ ConcurrencyIsHard.playground/Pages/Dispatch Groups.xcplaygroundpage/Contents.swift
@@ -12,20 +12,48 @@ import UIKit
 import PlaygroundSupport
 
 //: Create a new animation function on `UIView` that wraps an existing animation function, but now takes a dispatch group as well.
-// TODO
+extension UIView {
+  static func animateWithDuration(_ duration: TimeInterval, animations: () -> Void, group: DispatchGroup, completion: ((Bool) -> Void)?) {
+    group.enter()
+    animate(withDuration: duration, animations: animations) { (success) in
+      completion?(success)
+      group.leave()
+    }
+  }
+}
 
 //: Create a disptach group with `dispatch_group_create()`:
-// TODO
+let animationGroup = DispatchGroup()
 
 //: The animation uses the following views
-// TODO
+let view = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+view.backgroundColor = #colorLiteral(red: 0.6823074818, green: 0.08504396677, blue: 0.06545677781, alpha: 1)
+let box = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+box.backgroundColor = #colorLiteral(red: 0.4028071761, green: 0.7315050364, blue: 0.2071235478, alpha: 1)
+view.addSubview(box)
+
+PlaygroundPage.current.liveView = view
 
 //: The following completely independent animations now use the dispatch group so that you can determine when all of the animations have completed:
 
-// TODO
+UIView.animateWithDuration(1, animations: {
+  box.center = CGPoint(x: 150, y: 150)
+  }, group: animationGroup, completion: {
+    _ in
+    UIView.animateWithDuration(2, animations: {
+      box.transform = CGAffineTransform(rotationAngle: .pi/4)
+      }, group: animationGroup, completion: .none)
+})
+
+UIView.animateWithDuration(4, animations: {
+  view.backgroundColor = #colorLiteral(red: 0.9898764491, green: 0.7441664934, blue: 0.1842373013, alpha: 1)
+  }, group: animationGroup, completion: .none)
 
 
 //: `dispatch_group_notify()` allows you to specify a block that will be executed only when all the blocks in that dispatch group have completed:
-// TODO
+animationGroup.notify(queue: DispatchQueue.main) {
+  print("Animations Completed!")
+  //PlaygroundPage.current.finishExecution()
+}
 
 //: [➡ Thread safety with GCD Barriers](@next)
